b5e90d1f05478421cb6b0531154178a0934c0d21
Chamod-Ravishanka <197353560+Chamod-Ravishanka@users.noreply.github.com>
2025-06-29 03:18:11 +0530

    Updated safety-map.tsx

--- app/(tabs)/safety-map.tsx
+++ app/(tabs)/safety-map.tsx
@@ -22,7 +22,7 @@ import MapView, {
   Region 
 } from 'react-native-maps';
 import * as Location from 'expo-location';
-import { ArrowLeft, Navigation, Search, TriangleAlert as AlertTriangle, Shield, Droplets, Zap, Eye, MapPin, Layers, Filter, Target, X, Clock, Users, CheckCircle } from 'lucide-react-native';
+import { ArrowLeft, Navigation, Search, TriangleAlert as AlertTriangle, Shield, Droplets, Zap, Eye, MapPin, Layers, Filter, Target, X, Clock, Users, CircleCheck as CheckCircle } from 'lucide-react-native';
 
 const { width, height } = Dimensions.get('window');
 
